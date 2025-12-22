24dd52f858b4007b8da6687286abef2ccedfe899
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-12-22 23:15:40 +0500

    Update SocialSiderbar.tsx

--- app/components/SocialSiderbar.tsx
+++ app/components/SocialSiderbar.tsx
@@ -2,7 +2,6 @@
 import { motion } from "framer-motion";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import { SiKaggle } from "react-icons/si";
-
 const socialLinks = [
   { label: "GitHub", icon: <FaGithub size={20} />, url: "https://github.com/AbdulRehmanBaig384" },
   { label: "LinkedIn", icon: <FaLinkedin size={20} />, url: "https://www.linkedin.com/in/abdul-rehman-baig-/" },
@@ -29,11 +28,9 @@ export default function SocialSidebar() {
             className="text-sm font-medium text-white whitespace-nowrap">
             {item.label}
           </motion.span>
-
           <motion.div
             whileHover={{ scale: 1.15 }}
-            className="bg-gradient-to-br from-[#0ea5e9] to-[#7c3aed] text-white w-12 h-12 flex items-center justify-center rounded-full shadow-md transition-all duration-300 group-hover:shadow-[0_0_20px_rgba(14,165,233,0.8)]"
-          >
+            className="bg-gradient-to-br from-[#0ea5e9] to-[#7c3aed] text-white w-12 h-12 flex items-center justify-center rounded-full shadow-md transition-all duration-300 group-hover:shadow-[0_0_20px_rgba(14,165,233,0.8)]">
             {item.icon}
           </motion.div>
         </motion.a>
