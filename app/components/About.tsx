9040d47d3b55d83d3be2438ec745b73ce5e1dd8b
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-06-06 12:53:18 +0500

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -18,10 +18,10 @@ export default function About(){
       <div className="absolute inset-0 -z-10 bg-gradient-to-b from-blue-950/50 via-black to-blue-950/20" />
       <motion.h2
         initial={{opacity:0,y:-20 }}
-        whileInView={{ opacity:1, y: 0 }}
-        transition={{ duration:0.7,ease: "easeOut" }}
-        viewport={{ once: true }}
-          className=" mb-8 text-5xl font-extrabold bg-gradient-to-r from-[#00d4ff] via-[#60a5fa] to-[#7c3aed] bg-clip-text text-transparent drop-shadow-xl">
+        whileInView={{opacity:1, y: 0 }}
+        transition={{duration:0.7,ease:"easeOut"}}
+        viewport={{once:true}}
+          className="mb-8 text-5xl font-extrabold bg-gradient-to-r from-[#00d4ff] via-[#60a5fa] to-[#7c3aed] bg-clip-text text-transparent drop-shadow-xl">
         About Me
       </motion.h2>
       <motion.div
