22fe8757b66a8cece5c8d63a0ba05a6a933b2d4e
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2026-06-16 23:48:26 +0500

    Update ServicesSection.tsx

--- app/components/ServicesSection.tsx
+++ app/components/ServicesSection.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from "react";
+import React,{useState,useEffect}from "react";
 import { motion,Variants } from "framer-motion";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
